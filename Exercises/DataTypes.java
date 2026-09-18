8805dfd0bd0a70e18adde4e1509619b0b22adfbb
Monica Sanchez <iuumonis@gmail.com>
2026-09-18 11:40:10 -0600

    Solucion de DataTypes

--- Exercises/DataTypes.java
+++ Exercises/DataTypes.java
@@ -4,7 +4,23 @@ public class DataTypes {
     public String cadena;
     public int entero;
     public float flotante;
-    char caracter;
-    boolean boleano;
+    public char caracter;
+    public boolean boleano;
+
+    public DataTypes() {
+    this.cadena = "Hola Mundo";
+    this.entero = 10;
+    this.flotante = 5.9f;
+    this.caracter = 'A';
+    this.boleano = true;
+    }
+
+    public DataTypes(String cadena, int entero, float flotante, char caracter, boolean boleano){
+        this.cadena = cadena;
+        this.entero = entero;
+        this.flotante = flotante;
+        this.caracter = caracter;
+        this.boleano = boleano;
+    }
 
 }
